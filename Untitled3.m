758e68a4b28c73d63143112b0d330906a01910e0
peter anton <97073590+Peter-Anton@users.noreply.github.com>
2023-05-20 16:50:35 +0300

    remove untitled

--- Untitled3.m
+++
@@ -1,8 +0,0 @@
-function [a,b]=low_pass_filter(samplerate,cutoffFreq,order,filter_type)
-fm=samplerate/2;
-wn=cutoffFreq/fm;
-if (filter_type==0)
-    [a,b]=fir1(order,wn,'low');
-else
-    [a,b]=butter(order,wn);
-end
